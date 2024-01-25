533a3bd0a5297b7b043e3eb6d583c76c51760796
Fernando Castillo <desarrollo@freengers.com>
2024-01-25 12:04:26 -0600

    Composer install, preparing testing environment

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,11 +3,12 @@
 namespace Tests\Feature;
 
 use App\Models\User;
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic test example.
      */
